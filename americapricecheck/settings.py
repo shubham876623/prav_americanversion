3e7ad23707426cc86ede622b9c9becaab61aa8c5
shubham876623 <shubhamsaini8766@gmail.com>
2023-01-07 16:22:16 +0530

    added database

--- americapricecheck/settings.py
+++ americapricecheck/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'americapricecheck.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+#production database .....
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'product',
+        'USER': 'root',
+        'PASSWORD': '875596abcS@a',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
